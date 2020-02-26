e3bddec45818386ccb5f8d3361a770cb249e48ab
Joana Henriques <joanamarisacorreiahenriques@gmail.com>
2020-02-26 09:52:33 +0000

    [#1] Create WeatherForecast Swift project.

---
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherForecast
+//
+//  Created by Joana Henriques on 26/02/2020.
+//  Copyright © 2020 Joana Henriques. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
